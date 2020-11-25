34815f4f408b08403297f3f2b0603221d3b1055a
Karthik <CodingIsHeaven@gmail.com>
2020-11-25 13:40:44 +0530

    demo
    This is a demo

---
+++ Main.java
@@ -0,0 +1,15 @@
+package Mario;
+
+import java.awt.Dimension;
+
+public class Main {
+
+	public static final int WIDTH = 270;
+	public static final int HEIGHT = WIDTH/14*10;
+	public static final int SCALE = 4;
+	
+	public Main() {
+		Dimension size = new Dimension(WIDTH*SCALE,HEIGHT*SCALE);
+	}
+	
+}
